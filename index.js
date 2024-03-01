dd5d5929e30a0d9459396c4d2549e7ecc42494ae
unknown <bhumikakri2002@gmail.com>
2024-03-01 14:29:20 +0530

    completed

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // const express = require('express'); // cjs
 import express from "express";  // ESM
+import bodyParser from "body-parser";
 import fs from "fs";
-// import path from "path";
+import path from "path";
 import { nanoid } from "nanoid";
+import { fileURLToPath } from "url";
 // console.log(nanoid(8));
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// middileware
+app.use(bodyParser.urlencoded({ extended: true }));
+// middileware
 app.use(express.json());     //req.body me data dikhane k liye 
+
 app.get("/",(req,res)=>{
-    res.send(`<form action="/url-shortner" method="post">
-    <label for="longUrl">Enter a long URL:</label>
-    <input type="url" id="longUrl" name="longUrl" required>
-    <button type="submit">Shorten URL</button>
-  </form>`)
+    console.log("hello / gett");
+    res.sendFile(__dirname + "/index.html");
 })
 
 
@@ -30,23 +35,27 @@ app.post('/url-shortner', (req, res)=>{
     filedata[shortUrl] = longUrl
     fs.writeFileSync("urlmap.json",JSON.stringify(filedata));
 
-    res.json({
-        success:true,
-        message:`http://localhost:5000/${shortUrl}`
-    })
+    res.send(`Shortened URL: http://localhost:5000/${shortUrl}`);
+    // res.json({
+    //     success:true,
+    //     message:`http://localhost:5000/${shortUrl}`
+    // })
 })
 
 app.get('/:shortUrl', (req, res)=>{
     // console.log(req.params.shortUrl);
 
     const fileResponse = fs.readFileSync("urlmap.json");
-    // console.log(fileResponse);
     const filedata = JSON.parse(fileResponse.toString());
 
     const longUrl = filedata[req.params.shortUrl];
     // console.log(longUrl);
 
-   res.redirect(longUrl);
+    if (longUrl) {
+        res.redirect(longUrl);
+      } else {
+        res.status(404).send('URL not found');
+      }
 
 })
 
